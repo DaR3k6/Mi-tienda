7cc316340ff3b0ac869cb9a6042596ffd3e66f82
DaR3K6 <kevinvilleperez@gmail.com>
2023-05-18 12:33:13 -0500

    Optimizacion del codigo del backend

--- Back-tienda/modules/usuarios.js
+++ Back-tienda/modules/usuarios.js
@@ -1,9 +1,11 @@
 //Modulos requeridos para el proyecto
-
 const express = require("express");
 const cors = require("cors"); //Para evitar restricciones entre llamadas de sitios
 const usuario = express.Router(); //Trae el metodo router de express para hacer los endpoints
 const conex = require("./bdatos.js");
+const bycript = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util"); //La trae por defecto NODE JS me permite usar async/await opcion a fetch
 
 //Construimos la capa intermedia de la aplicacion MIDDLEWARE
 usuario.use(express.json()); //Serializa la data en JSON
@@ -21,7 +23,23 @@ const campoUsuario = [
   "telefono",
   "esAdmin",
 ];
-//Verbo GET LISTAR
+
+usuario.get("/usuarios", async (req, res) => {
+  try {
+    conex.query(
+      "SELECT idUsuario,nombre,email FROM usuario; ",
+      (error, respuesta) => {
+        console.log(respuesta);
+        res.send(respuesta);
+      }
+    );
+  } catch (error) {
+    //throw error;
+    console.log(error);
+  }
+});
+
+/* //Verbo GET LISTAR
 usuario.get("/usuarios", (req, res) => {
   conex.query("SELECT * FROM usuario", (error, respuesta) => {
     if (error) {
@@ -30,21 +48,61 @@ usuario.get("/usuarios", (req, res) => {
       res.send(respuesta);
     }
   });
+}) */
+
+//Verbo POST INSERTAR USUARIO
+usuario.post("/usuarios", async (req, res) => {
+  try {
+    let data = {};
+    campoUsuario.forEach(campo => {
+      if (campo === "constraseña") {
+        data[campo] = bycript.hashSync(req.body[campo]);
+      } else {
+        data[campo] = req.body[campo];
+      }
+    });
+    conex.query("INSERT INTO usuario SET ?", data, (error, respuesta) => {
+      console.log(`Insercion correcta ${respuesta}`);
+      res.status(201).send(respuesta);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send(error);
+  }
 });
 
-//Verbo POST INSERTAR
-usuario.post("/usuarios", (req, res) => {
-  let data = {};
-  campoUsuario.forEach(campo => {
-    data[campo] = req.body[campo];
-  });
-  conex.query("INSERT INTO usuario SET ?", data, (error, respuesta) => {
-    if (error) {
-      console.log(error);
+//Login de Usuario
+usuario.post("/login", async (req, res) => {
+  try {
+    const email = req.body["email"];
+    const constraseña = req.body["constraseña"]; // Utilizamos el nombre correcto
+    //Validamos que llegen los datos completos
+    if (!email || !constraseña) {
+      console.log("Debe enviar los datos completos");
     } else {
-      res.status(201).send(respuesta);
+      conex.query(
+        "SELECT * FROM usuario WHERE email = ? ",
+        [email],
+        async (error, respuesta) => {
+          if (
+            respuesta.length == 0 ||
+            !(await bycript.compare(constraseña, respuesta[0].constraseña)) // Utilizamos el nombre correcto
+          ) {
+            console.log(
+              "El usuario y/o la clave ingresado no existen en la aplicación"
+            );
+          } else {
+            //Enviamos las variables al frontend para que cargue la página
+            console.log("BIENVENIDO AL SISTEMA DE INFORMACIÓN ");
+            res.status(200).send(respuesta);
+          }
+        }
+      );
     }
-  });
+  } catch (error) {
+    console.log("Hay un error en la conexión con el servidor");
+    res.status(404).send(error);
+  }
 });
 
 //Verbo PUT ACUTALIZAR
